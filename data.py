865e774b3635a12d5169b42b7d5ad0aae2f86bbb
072jiajia <63368701+072jiajia@users.noreply.github.com>
2020-11-13 20:57:54 +0800

    Add files via upload

---
+++ data.py
@@ -0,0 +1,128 @@
+import os
+import sys
+import glob
+import h5py
+import numpy as np
+import torch
+from torch.utils.data import Dataset, BatchSampler
+
+# os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
+def load_mask(partition, permute):
+    raw_path='/home/kschen/dgcnn_pytorch/dgcnn.pytorch/data/h5_files/main_split'
+    data = h5py.File("{}/{}_objectdataset_{}.h5".format(raw_path, partition, permute), 'r+')
+    mask = data['mask']
+    mask[mask != -1] = 1
+    mask[mask == -1] = 0 
+    return mask
+    
+class ScanObject_coseg(Dataset):
+    """Scan object dataset
+    permute: ['augmented25rot', 'augmented25_norot', 'augmentedrot', 'augmentedrot_scale75', 'raw']
+    obj: {0: 'bag', 1: 'bin', 2: 'box', 3: 'cabinet', 4: 'chair', 5: 'desk',
+          6: 'display', 7: 'door', 8: 'shelf', 9: 'table', 10: 'bed', 11: 'pillow',
+          12: 'sink', 13: 'sofa', 14: 'toilet', 15: 'all'}
+    """
+    def __init__(self,raw_path='data/scanobjectnn/main_split', n_points=1024, 
+             partition='training', permute='raw', obj=15, label_binarize=True, norm=True, center=True):       
+        cat_to_label = {0: 'bag', 1: 'bin', 2: 'box', 3: 'cabinet', 4: 'chair', 5: 'desk',
+                        6: 'display', 7: 'door', 8: 'shelf', 9: 'table', 10: 'bed', 11: 'pillow',
+                        12: 'sink', 13: 'sofa', 14: 'toilet', 15: 'all'}
+
+        self.partition = partition
+        self.n_points = n_points
+        self.norm = norm
+        self.center = center
+
+        if permute == 'raw':
+            data = h5py.File("{}/{}_objectdataset.h5".format(raw_path, partition), 'r+')
+        else:
+            data = h5py.File("{}/{}_objectdataset_{}.h5".format(raw_path, partition, permute), 'r+')    
+            
+        self.points = np.array(data['data'][:].astype('float32'))
+        self.labels = np.array(data['label'][:].astype('int64'))
+        self.masks = np.array(data['mask'][:].astype('float32'))
+        
+        data.close()
+        
+        select_label = cat_to_label[obj]
+
+        if select_label != 'all':
+            self.points = self.points[self.labels == obj]
+            self.masks = self.masks[self.labels == obj]
+            self.labels = self.labels[self.labels == obj]
+        if label_binarize:
+            self.masks[self.masks != -1] = 1
+            self.masks[self.masks == -1] = 0  
+        else:
+            self.masks[:, :] = 1 # 0 stands for BG
+        
+        # self.points = normalize(self.points)
+            
+        print("Number of data:", self.points.shape[0])
+        
+    def __getitem__(self, item):
+        coord = self.points[item][:self.n_points]
+        label = self.labels[item]
+        mask = self.masks[item][:self.n_points]
+
+        if self.partition == 'training':
+            # Shuffle the points to do random sampling
+            perm_index = np.random.permutation(self.points.shape[1])
+            self.points[item] = self.points[item, perm_index]
+            self.masks[item] = self.masks[item, perm_index]
+
+        if self.partition == 'training':
+            coord = translate_pointcloud(coord)
+
+        if self.partition == 'test':
+            # You can delete the following line to do random sampling
+            RuntimeError('Please Delete This Line Or Do The Farthest Point Sampling Here')
+            pass
+
+        return coord, label, mask
+
+    def __len__(self):
+        return len(self.points)
+
+
+def normalize(PointClouds, epsilon=1e-8):
+    # Not Suggest
+    Centers = np.mean(PointClouds, axis=1, keepdims=True)
+    PointClouds -= Centers
+    Vars = np.mean(PointClouds ** 2, axis=(1, 2), keepdims=True) + epsilon
+    PointClouds /= np.sqrt(Vars)
+    return PointClouds
+
+
+def translate_pointcloud(pointcloud):
+    xyz1 = np.random.uniform(low=2./3., high=3./2., size=[3])
+    xyz2 = np.random.uniform(low=-0.2, high=0.2, size=[3])
+       
+    translated_pointcloud = np.add(np.multiply(pointcloud, xyz1), xyz2).astype('float32')
+    return translated_pointcloud
+
+class TrainingBatchSampler(BatchSampler):
+    ''' Batch Sampler
+    '''
+
+    def __init__(self, dataset, n_points_choices, batch_size):
+        self.dataset = dataset
+        self.choices = n_points_choices
+        self.batch_size = batch_size
+
+    def __iter__(self):
+        ''' Randomly set the number of points '''
+        # initialize yield count and object indices
+        count = 0
+        obj_idx = np.random.permutation(len(self.dataset))
+        while count + self.batch_size < len(self.dataset):
+            # Randomly set the number of sample points
+            n_points = np.random.choice(self.choices, 1)[0]
+            self.dataset.n_points = n_points
+            
+            # yield the index of object to data_loader
+            yield obj_idx[count: count + self.batch_size]
+            count += self.batch_size
+
+    def __len__(self):
+        return len(self.dataset) // self.batch_size
---
+++ exp_classification.py
@@ -0,0 +1,226 @@
+from __future__ import print_function
+import os
+import argparse
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from sklearn.metrics import accuracy_score, balanced_accuracy_score
+
+# custom module
+from model import DGCNN_cls
+from util import cal_loss, IOStream
+from data import ScanObject_coseg, TrainingBatchSampler
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0, 1, 2, 3, 4"
+
+
+def _init_():
+    if not os.path.exists('checkpoints'):
+        os.makedirs('checkpoints')
+    if not os.path.exists('checkpoints/'+args.path):
+        os.makedirs('checkpoints/'+args.path)
+    if not os.path.exists('checkpoints/'+args.path+'/'+'models'):
+        os.makedirs('checkpoints/'+args.path+'/'+'models')
+    os.system('cp exp_classification.py checkpoints'+'/' +
+              args.path+'/'+'exp_classification.py.backup')
+    os.system('cp model.py checkpoints' + '/' +
+              args.path + '/' + 'model.py.backup')
+    os.system('cp util.py checkpoints' + '/' +
+              args.path + '/' + 'util.py.backup')
+    os.system('cp data.py checkpoints' + '/' +
+              args.path + '/' + 'data.py.backup')
+
+
+def train(args, io, model, train_loader, opt):
+    # I will NEVER train the model on a CPU so I simply set "cuda"
+    device = torch.device("cuda")
+
+    criterion = cal_loss
+    train_loss = 0.0
+    count = 0.0
+    model.train()
+    train_pred = []
+    train_true = []
+    for data, label, mask in train_loader:
+        # load data
+        data, label = data.to(device), label.to(device).squeeze()
+        data = data.permute(0, 2, 1)
+        batch_size = data.size()[0]
+
+        # forward propagation and back propagation
+        opt.zero_grad()
+        logits = model(data)
+        loss = criterion(logits, label)
+        loss.backward()
+        opt.step()
+
+        # record results
+        preds = logits.max(dim=1)[1]
+        count += batch_size
+        train_loss += loss.item() * batch_size
+        train_true.append(label.cpu().numpy())
+        train_pred.append(preds.detach().cpu().numpy())
+
+    # record results
+    train_true = np.concatenate(train_true)
+    train_pred = np.concatenate(train_pred)
+    io.cprint('Train  '
+              'loss: %.6f, '
+              'train acc: %.6f, '
+              'train avg acc: %.6f'
+              % (train_loss / count,
+                 accuracy_score(train_true, train_pred),
+                 balanced_accuracy_score(train_true, train_pred)))
+
+    return
+
+
+def test(args, io, model, test_loader, test_name):
+    with torch.no_grad():
+        device = torch.device("cuda")
+        model = model.eval()
+
+        # initialize the parameters
+        criterion = cal_loss
+        test_loss = 0.0
+        count = 0.0
+        model.eval()
+        test_pred = []
+        test_true = []
+        for data, label, mask in test_loader:
+            # load data
+            data, label = data.to(device), label.to(device).squeeze()
+            data = data.permute(0, 2, 1)
+            batch_size = data.size()[0]
+
+            # predict
+            logits = model(data)
+            loss = criterion(logits, label)
+            preds = logits.max(dim=1)[1]
+
+            # record results
+            count += batch_size
+            test_loss += loss.item() * batch_size
+            test_true.append(label.cpu().numpy())
+            test_pred.append(preds.detach().cpu().numpy())
+
+        # record results
+        test_true = np.concatenate(test_true)
+        test_pred = np.concatenate(test_pred)
+        test_acc = accuracy_score(test_true, test_pred)
+        avg_per_class_acc = balanced_accuracy_score(test_true, test_pred)
+
+        io.cprint(' * %s '
+                  'loss: %.6f, '
+                  'test acc: %.6f, '
+                  'test avg acc: %.6f'
+                  % (test_name,
+                     test_loss / count,
+                     test_acc,
+                     avg_per_class_acc))
+
+    return test_acc
+
+
+def experiment(n_points_choices, path):
+    args.n_points_choices = n_points_choices
+    args.path = path
+
+    # make path
+    _init_()
+
+    # record args
+    io = IOStream('checkpoints/' + args.path + '/run.log')
+    io.cprint(str(args))
+
+    io.cprint('Using GPU : ' + str(torch.cuda.current_device()) +
+              ' from ' + str(torch.cuda.device_count()) + ' devices')
+
+    # set seeds
+    torch.manual_seed(1)
+    torch.cuda.manual_seed(1)
+
+    # initialize train_loader
+    train_dataset = ScanObject_coseg(partition='training')
+    train_sampler = TrainingBatchSampler(train_dataset,
+                                         args.n_points_choices,
+                                         args.batch_size)
+    train_loader = DataLoader(train_dataset,
+                              batch_sampler=train_sampler,
+                              num_workers=16)
+
+    # initialize test data loaders
+    test_dataset256 = ScanObject_coseg(partition='test', n_points=256)
+    test_loader256 = DataLoader(test_dataset256, num_workers=16,
+                                batch_size=args.test_batch_size)
+    test_dataset512 = ScanObject_coseg(partition='test', n_points=512)
+    test_loader512 = DataLoader(test_dataset512, num_workers=16,
+                                batch_size=args.test_batch_size)
+    test_dataset1024 = ScanObject_coseg(partition='test', n_points=1024)
+    test_loader1024 = DataLoader(test_dataset1024, num_workers=16,
+                                 batch_size=args.test_batch_size)
+    test_dataset2048 = ScanObject_coseg(partition='test', n_points=2048)
+    test_loader2048 = DataLoader(test_dataset2048, num_workers=16,
+                                 batch_size=args.test_batch_size)
+
+    # Load models
+    device = torch.device("cuda")
+    model = DGCNN_cls(args).to(device)
+    model = nn.DataParallel(model)
+
+    # Use SGD and CosineAnnealingLR to train
+    print("Use SGD")
+    opt = optim.SGD(model.parameters(), lr=args.lr,
+                    momentum=args.momentum, weight_decay=1e-4)
+    scheduler = CosineAnnealingLR(opt, args.epochs, eta_min=1e-3)
+
+    # start training
+    print("Let's use", torch.cuda.device_count(), "GPUs!")
+    for i in range(args.epochs):
+        io.cprint('Epoch [%d]' % (i + 1))
+        # train model
+        train(args, io, model, train_loader, opt)
+
+        # adjust learning rate
+        scheduler.step()
+
+        # test
+        test(args, io, model, test_loader256, 'Test 256 ')
+        test(args, io, model, test_loader512, 'Test 512 ')
+        test(args, io, model, test_loader1024, 'Test 1024')
+        test(args, io, model, test_loader2048, 'Test 2048')
+
+
+if __name__ == "__main__":
+    # Training settings
+    parser = argparse.ArgumentParser(description='Point Cloud Recognition')
+    parser.add_argument('--exp_name', type=str, default='exp', metavar='N',
+                        help='Name of the experiment')
+    parser.add_argument('--dataset', type=str, default='scanobjectnn', metavar='N',
+                        choices=['scanobjectnn'])
+    parser.add_argument('--batch_size', type=int, default=128, metavar='batch_size',
+                        help='Size of batch)')
+    parser.add_argument('--test_batch_size', type=int, default=32, metavar='batch_size',
+                        help='Size of batch)')
+    parser.add_argument('--epochs', type=int, default=250, metavar='N',
+                        help='number of episode to train ')
+    parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
+                        help='learning rate')
+    parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                        help='SGD momentum (default: 0.9)')
+    parser.add_argument('--dropout', type=float, default=0.5,
+                        help='initial dropout rate')
+    parser.add_argument('--emb_dims', type=int, default=1024, metavar='N',
+                        help='Dimension of embeddings')
+    parser.add_argument('--k', type=int, default=20, metavar='N',
+                        help='Num of nearest neighbors to use')
+    args = parser.parse_args()
+
+    experiment([256], 'train256')
+    experiment([512], 'train512')
+    experiment([1024], 'train1024')
+    experiment([2048], 'train2048')
+    experiment([256, 512, 1024, 2048], 'train_mix')
